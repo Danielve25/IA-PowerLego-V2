9760532c197df7553ddc1395c77869fcfb3f1be3
Danielve25 <danielvelascocuentaestudiantil@gmail.com>
2025-02-15 13:38:04 -0500

    cambiar modelo de gemini

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,9 +1,9 @@
 import React, {useState, useEffect} from "react";
 import {GoogleGenerativeAI} from "@google/generative-ai";
 import axios from "axios";
-//const AIModel = 'gemini-1.5-flash'
+const AIModel = 'gemini-1.5-flash'
 //const AIModel = "gemini-2.0-flash";
-const AIModel = "gemini-1.5-flash-8b";
+//const AIModel = "gemini-1.5-flash-8b";
 const API_KEY_GOOGLE = import.meta.env.VITE_GOOGLE_API_KEY;
 const API_KEY_THINGSPEAK = import.meta.env.VITE_THINGSPEAK_API_KEY;
 
@@ -16,15 +16,15 @@ const ChatBot = () => {
     useEffect(() => {
         const fetchData = () => {
             axios
-        .get(`https://api.thingspeak.com/channels/2736935/feeds.json?api_key=${API_KEY_THINGSPEAK}&results=1`)
-        .then(({data}) => {
-            console.log(data.channel);
-            console.log(data.feeds[0]);
-            setChannelData(data.channel);
-            setDatosSensores(data.feeds[0]);
-        })
-        .catch((err) => console.log("error al obtener datos"));
-        }
+            .get(`https://api.thingspeak.com/channels/2736935/feeds.json?api_key=${API_KEY_THINGSPEAK}&results=1`)
+            .then(({data}) => {
+                console.log(data.channel);
+                console.log(data.feeds[0]);
+                setChannelData(data.channel);
+                setDatosSensores(data.feeds[0]);
+            })
+            .catch((err) => console.log("error al obtener datos"));
+        };
         fetchData();
         const interval = setInterval(fetchData, 1000);
         return () => clearInterval(interval);
@@ -33,7 +33,8 @@ const ChatBot = () => {
     const [input, setInput] = useState("");
 
     const sendMessage = async () => {
-        const context =`
+        const context =
+            `
             Actúa como un experto en vida marina. Te proporcionaré datos sobre un lugar y algunas condiciones ambientales. Usando solo esa información, y basándote en tu conocimiento enciclopédico sobre cada especie marina, determina si una especie específica (que te indicaré) podría sobrevivir en ese entorno. Responde con un 'sí' 'probablemnte si' 'no' 'probablemente no', seguido de una breve explicación que justifique tu respuesta.
             Datos
             Temperatura: ${DatosSensores.field1} °C
@@ -41,25 +42,14 @@ const ChatBot = () => {
             Temperatura del agua: ${DatosSensores.field3} °C
             Ubicación: ${DatosSensores.field5}, ${DatosSensores.field4}
             Está lloviendo: ${DatosSensores.field6 === "0" ? "no" : "sí"}
-            Especie: te lo vaan a ddecir la gente que te pregunta
             Importante: La persona que te pregunta no tiene conocimientos sobre el ambiente marino, así que tu respuesta debe ser clara y concisa."
-            
-            Ejemplo de uso
-            Ubicación (coordenadas): 3.42157, -76.49651
-            Temperatura ambiente: 26.60°C
-            Humedad: 46.50%
-            Temperatura del agua: 25.56°C
-            Está lloviendo: No
-            Especie marina: Tiburón martillo
-
-            \n` +
-            messages.map((msg) => `Tú: ${msg.user}\nBot: ${msg.bot}`).join("\n");
+            \n` + messages.map((msg) => `Tú: ${msg.user}\nBot: ${msg.bot}`).join("\n");
         const prompt = `${context}\nTú: ${input}`;
         const result = await model.generateContent(prompt, {language: "es"});
         setMessages([...messages, {user: input, bot: result.response.text()}]);
         setInput("");
     };
-
+    console.log(messages)
     return (
         <div>
             <div>
