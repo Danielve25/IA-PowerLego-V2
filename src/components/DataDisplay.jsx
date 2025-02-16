da34550a60db37d7c2dd8b84fe462998ae545326
Danielve25 <danielvelascocuentaestudiantil@gmail.com>
2025-02-15 22:06:38 -0500

    añadir estilos al display de datos V3

--- src/components/DataDisplay.jsx
+++ src/components/DataDisplay.jsx
@@ -1,7 +1,12 @@
 import {React, useEffect, useState} from "react";
 import axios from "axios";
+import TermoIcon from './icons/TermometroIcon'
+import HumidityIcon from "./icons/HumidityIcon";
+import CloudIcon from "./icons/CloudIcon";
+import CloudIconRain from "./icons/CloudIconRain";
+import WaterTemperatureIcon from "./icons/WaterTemperatureIcon";
 const API_KEY_THINGSPEAK = import.meta.env.VITE_THINGSPEAK_API_KEY;
-
+import MapIcon from "./icons/MapIcon";
 const DataDisplay = () => {
     const [AllSensor, setAllSensor] = useState(null);
     const [ChannelData, setChannelData] = useState(null)
@@ -34,11 +39,11 @@ const DataDisplay = () => {
             )}
             {AllSensor && (
                 <ul>
-                    <li>ubicacion: {`${AllSensor.field4}, ${AllSensor.field5}`}</li>
-                    <li>temperatura: {parseFloat(AllSensor.field1).toFixed(2)}</li>
-                    <li>humedad: {parseFloat(AllSensor.field2).toFixed(2)}</li>
-                    <li>temperatura del aguaa: {parseFloat(AllSensor.field3).toFixed(2)}</li>
-                    <li>esta lloviendo: {AllSensor.field6 === "0" ? "no" : "sí"}</li>
+                    <li><MapIcon fill="#007BFF" height={20}/> ubicacion: {`${AllSensor.field4}, ${AllSensor.field5}`}</li>
+                    <li><TermoIcon height={30}/> temperatura: {parseFloat(AllSensor.field1).toFixed(2)}</li>
+                    <li><HumidityIcon height={20}/> humedad: {parseFloat(AllSensor.field2).toFixed(2)}</li>
+                    <li><WaterTemperatureIcon height={30} /> temperatura del agua: {parseFloat(AllSensor.field3).toFixed(2)}</li>
+                    <li>{AllSensor.field6 === "0" ? <><CloudIcon/> esta lloviendo: no</> : <><CloudIconRain width={20}/> esta lloviendo: si</>}</li>
                 </ul>
             )}
         </div>
